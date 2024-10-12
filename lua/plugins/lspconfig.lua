e1eb366e2222549283c8231a3863cce7cb10ccb8
Sven Blanke <blanke175@gmail.com>
2024-10-12 16:05:46 +0200

    added diagnostics on hover

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,6 +1,10 @@
-vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.declaration()<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", { noremap = true, silent = true })
 
+vim.diagnostic.config({
+    virtual_text = false,
+})
+vim.o.updatetime = 250
 return {
     "neovim/nvim-lspconfig",
     event = "VeryLazy",
@@ -26,37 +30,39 @@ return {
             function(server_name)
                 lspconfig[server_name].setup({
                     capabilities = lsp_capabilities,
+                    on_attach = function(_, bufnr)
+                        vim.api.nvim_create_autocmd("CursorHold", {
+                            buffer = bufnr,
+                            callback = function()
+                                local opts = {
+                                    focusable = false,
+                                    close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+                                    border = "rounded",
+                                    source = "always",
+                                    prefix = " ",
+                                    scope = "cursor",
+                                }
+                                vim.diagnostic.open_float(nil, opts)
+                            end,
+                        })
+                    end,
+                    settings = {
+                        Lua = {
+                            diagnostics = {
+                                -- Get the language server to recognize the `vim` global
+                                globals = { "vim" },
+                            },
+                        },
+                        pylsp = {
+                            plugins = {
+                                pycodestyle = {
+                                    maxLineLength = 200,
+                                },
+                            },
+                        },
+                    },
                 })
             end,
         })
-
-        lspconfig.pylsp.setup({
-            settings = {
-                pylsp = {
-                    plugins = {
-                        pycodestyle = {
-                            maxLineLength = 200,
-                        },
-                    },
-                },
-            },
-        })
-
-        lspconfig.lua_ls.setup({
-            settings = {
-                Lua = {
-                    diagnostics = {
-                        -- Get the language server to recognize the `vim` global
-                        globals = { "vim" },
-                    },
-                },
-            },
-        })
-
-        function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-            opts = opts or {}
-            opts.border = opts.border or "rounded" -- Set border to rounded
-            return open_floating_preview(contents, syntax, opts, ...)
-        end
     end,
 }
